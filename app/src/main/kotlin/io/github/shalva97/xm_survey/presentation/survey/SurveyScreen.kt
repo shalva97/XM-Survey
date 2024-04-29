d2572dd521efc45c8475cf48706f466ecc76fc67
shalva97 <shalva258@gmail.com>
2024-04-29 17:44:42 +0400

    Added pager and Question composable

--- app/src/main/kotlin/io/github/shalva97/xm_survey/presentation/survey/SurveyScreen.kt
+++ app/src/main/kotlin/io/github/shalva97/xm_survey/presentation/survey/SurveyScreen.kt
@@ -1,26 +1,49 @@
 package io.github.shalva97.xm_survey.presentation.survey
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import io.github.shalva97.xm_survey.presentation.survey.components.Question
 import io.github.shalva97.xm_survey.presentation.survey.components.QuestionsSubmitted
 import io.github.shalva97.xm_survey.presentation.survey.components.SurveyTopBar
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SurveyScreen(
     viewModel: SurveyViewModel = hiltViewModel(),
 ) {
     Column(Modifier.fillMaxSize()) {
-        SurveyTopBar()
+        val pagerState = rememberPagerState(pageCount = { 10 })
+        val coroutineScope = rememberCoroutineScope()
+        SurveyTopBar(onPreviousClicked = {
+            coroutineScope.launch {
+                pagerState.animateScrollToPage(page = pagerState.currentPage - 1)
+            }
+        }, onNextClicked = {
+            coroutineScope.launch {
+                pagerState.animateScrollToPage(page = pagerState.currentPage + 1)
+            }
+        })
         QuestionsSubmitted()
+        HorizontalPager(state = pagerState) { page ->
+            Question("asdasd question $page")
+        }
     }
 }
 
 @Preview
 @Composable
 fun SurveyScreenPreview() {
-    SurveyScreen()
+    Surface {
+        SurveyScreen()
+    }
 }
---
+++ app/src/main/kotlin/io/github/shalva97/xm_survey/presentation/survey/components/Question.kt
@@ -0,0 +1,67 @@
+package io.github.shalva97.xm_survey.presentation.survey.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.shalva97.xm_survey.R
+
+@Composable
+fun Question(
+    question: String,
+    onSubmitClicked: (answer: String) -> Unit = {},
+    isAlreadySubmitted: Boolean = false,
+) {
+    Column(Modifier.fillMaxWidth()) {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = question,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.titleLarge
+        )
+        var text by remember { mutableStateOf("") }
+        TextField(modifier = Modifier
+            .padding(top = 16.dp)
+            .fillMaxWidth(),
+            value = text,
+            onValueChange = { if (it.length <= MAX_ANSWER_LENGTH) text = it },
+            label = { Text(text = stringResource(R.string.type_here_for_an_answer)) })
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(.5f)
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 16.dp), onClick = { onSubmitClicked.invoke(text) }
+        ) {
+            if (isAlreadySubmitted) {
+                Text(text = stringResource(R.string.already_submitted))
+            } else {
+                Text(text = stringResource(R.string.submit))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun QuestionPreview() {
+    Surface {
+        Question("What is your favourite food?")
+    }
+}
+
+private const val MAX_ANSWER_LENGTH = 200
